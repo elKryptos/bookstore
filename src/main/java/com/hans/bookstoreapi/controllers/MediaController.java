4f3256b66eeed6b6bdf9cdc45aad36a8b9f4b156
elKryptos <154229030+elKryptos@users.noreply.github.com>
2024-07-21 12:45:17 +0200

    Added endpoint for loading and show a png file

--- src/main/java/com/hans/bookstoreapi/controllers/MediaController.java
+++ src/main/java/com/hans/bookstoreapi/controllers/MediaController.java
@@ -3,11 +3,14 @@ package com.hans.bookstoreapi.controllers;
 import com.hans.bookstoreapi.responses.UploadMediaResponse;
 import com.hans.bookstoreapi.services.StorageService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
 
 @AllArgsConstructor
 @RequestMapping("/api/media")
@@ -20,6 +23,13 @@ public class MediaController {
     public UploadMediaResponse path(@RequestParam("file") MultipartFile multipartFile){
         String path = storageService.store(multipartFile);
         return new UploadMediaResponse(path);
+    }
+
+    @GetMapping("/{filename}")
+    public ResponseEntity<Resource> getResource(@PathVariable String filename) throws IOException {
+        Resource resource = storageService.loadAsResource(filename);
+        String contentType =  Files.probeContentType(resource.getFile().toPath());
+        return ResponseEntity.status(200).header(HttpHeaders.CONTENT_TYPE, contentType).body(resource);
     }
 
 }
